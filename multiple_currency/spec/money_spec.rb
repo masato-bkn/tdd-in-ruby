7117c4a448a0b07206ea5d1484719ff2ed96fd8f
重田 将人 <masato.0928.j@gmail.com>
2021-04-02 21:08:30 +0900

    specをdscribeで切り分けた

--- multiple_currency/spec/money_spec.rb
+++ multiple_currency/spec/money_spec.rb
@@ -1,29 +1,37 @@
 # frozen_string_literal: true
 
-require_relative './../dollar'
-require_relative './../franc'
+require_relative './../money'
 
-RSpec.describe 'Dollar' do
-  it '正しく計算できること' do
-    five = Money.dollar(5)
-    product = five.times(2)
-    expect(product.equal(Money.dollar(10))).to be_truthy
-
-    product = five.times(3)
-    expect(product.equal(Money.dollar(15))).to be_truthy
+describe 'Money' do
+  describe 'times' do
+    it '5ドル * 2 = 10ドル' do
+      five = Money.dollar(5)
+      product = five.times(2)
+      expect(product.equal(Money.dollar(10))).to be_truthy
+    end
   end
 
-  it '比較できること' do
-    expect(Money.dollar(5).equal(Money.dollar(5))).to be_truthy
-    expect(Money.dollar(5).equal(Money.dollar(6))).to be_falsey
-    expect(Money.dollar(5).equal(5)).to be_falsey
+  describe 'equals' do
+    it '5ドル = 5ドル' do
+      expect(Money.dollar(5).equal(Money.dollar(5))).to be_truthy
+    end
+
+    it '5ドル != 6ドル' do
+      expect(Money.dollar(5).equal(Money.dollar(6))).to be_falsey
+    end
+
+    it '5ドル != 5' do
+      expect(Money.dollar(5).equal(5)).to be_falsey
+    end
+
+    it '5ドル = 6フランク' do
+      expect(Money.dollar(5).equal(Money.franc(5))).to be_falsey
+    end
   end
 
-  it do
-    expect(Money.dollar(5).equal(Money.franc(5))).to be_falsey
-  end
-
-  it do
-    expect(Money.dollar(5).currency).to eq('USD')
+  describe 'currency' do
+    it do
+      expect(Money.dollar(5).currency).to eq('USD')
+    end
   end
 end
